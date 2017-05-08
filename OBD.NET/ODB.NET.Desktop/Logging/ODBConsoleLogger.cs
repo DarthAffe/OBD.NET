ede7de0411253ec5a5d24f82b79fc3cbd060ab26
Roman Lumetsberger <roman.lumetsberger@outlook.com>
2017-05-08 21:57:32 +0200

    Added ConsoleClient example

---
+++ OBD.NET/ODB.NET.Desktop/Logging/ODBConsoleLogger.cs
@@ -0,0 +1,35 @@
+﻿using OBD.NET.Logging;
+using System;
+using System.Diagnostics;
+
+namespace OBD.NET.Common.Logging
+{
+    /// <summary>
+    /// Simple console logger
+    /// </summary>
+    /// <seealso cref="OBD.NET.Logging.IOBDLogger" />
+    public class OBDConsoleLogger : IOBDLogger
+    {
+        public OBDLogLevel LogLevel { get; set; }
+
+        public OBDConsoleLogger()
+        {
+            LogLevel = OBDLogLevel.None;
+        }
+
+        public OBDConsoleLogger(OBDLogLevel level)
+        {
+            LogLevel = level;
+        }
+
+        public void WriteLine(string text, OBDLogLevel level)
+        {
+            if (LogLevel == OBDLogLevel.None) return;
+
+            if((int)level >= (int)LogLevel)
+            { 
+                Console.WriteLine($"{DateTime.Now.ToString()} -  {level} -  {text}");
+            }
+        }
+    }
+}
